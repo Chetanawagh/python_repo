7948fb93129106b3bf8b49bea923d4ac0206d9f7
chetanawagh <waghchetana12@gmail.com>
2023-05-30 10:52:13 +0530

    app.py changed

--- src/app.py
+++ src/app.py
@@ -1,21 +1,34 @@
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-# @app.route("/")
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username == 'admin' and password == 'password':
-            return 'Login Successful!'
-        else:
-            return 'Login Failed!'
-    # asd= render_template('home.html')
-    # print(asd)
+# @app.route('/', methods=['GET', 'POST'])
+# # @app.route("/")
+# def login():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         if username == 'admin' and password == 'password':
+#             return 'Login Successful!'
+#         else:
+#             return 'Login Failed!'
+#     # asd= render_template('home.html')
+#     # print(asd)
 
-    return render_template('home.html')
+#     return render_template('home.html')
+
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello from Flask!'
+
+
+@app.route('/')
+def test():
+    return 'Testing pythonanywhere!'
 
 if __name__ == '__main__':
     print('start program')
